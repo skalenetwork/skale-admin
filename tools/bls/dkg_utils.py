730cc8c51f266a6ea1bfa22dda185219fa09bd11
Oleh Nikolaiev <oleg@skalelabs.com>
2019-10-22 16:11:19 +0300

    SKALE-1669-fix-node-ids

--- tools/bls/dkg_utils.py
+++ tools/bls/dkg_utils.py
@@ -28,23 +28,28 @@ def init_dkg_client(schain_config_filepath, web3, wallet, n, t):
     with open(schain_config_filepath, 'r') as infile:
         config_file = json.load(infile)
 
-    node_id = config_file["skaleConfig"]["nodeInfo"]["nodeID"]
+    node_id_dkg = -1
+    node_id_contract = config_file["skaleConfig"]["nodeInfo"]["nodeID"]
     public_keys = [0] * n
     i = 0
-    node_ids = [0] * n
+    node_ids = dict()
     is_node_id_set = False
     for node in config_file["skaleConfig"]["sChain"]["nodes"]:
-        if node["nodeID"] == node_id and not is_node_id_set:
-            node_id = i
-            is_node_id_set = True
+        if node["nodeID"] == config_file["skaleConfig"]["nodeInfo"]["nodeID"]:
+            node_id_dkg = i
 
-        node_ids[i] = node["nodeID"]
+        node_ids_contract = dict()
+        node_ids_dkg = dict()
+
+        node_ids_contract[node["nodeID"]] = i
+        node_ids_dkg[i] = node["nodeID"]
+
         public_keys[i] = coincurve.PublicKey(bytes.fromhex("04" + node["publicKey"]))
         i += 1
 
     schain_name = config_file["skaleConfig"]["sChain"]["schainName"]
 
-    dkg_client = DKGClient(node_id, web3, wallet, t, n, schain_name, public_keys, node_ids)
+    dkg_client = DKGClient(node_id, node_id_contract, web3, wallet, t, n, schain_name, public_keys, node_ids_dkg, node_ids_contract)
     return dkg_client
 
 def broadcast(dkg_client, web3):
@@ -53,39 +58,39 @@ def broadcast(dkg_client, web3):
 def send_complaint(dkg_client, index, web3):
     dkg_client.SendComplaint(index, get_dkg_contract(web3))
 
-def response(dkg_client, fromNodeIndex, web3):
-    dkg_client.Response(get_dkg_contract(web3), fromNodeIndex)
+def response(dkg_client, web3):
+    dkg_client.Response(get_dkg_contract(web3))
 
 def send_allright(dkg_client, web3):
     dkg_client.Allright(get_dkg_contract(web3))
 
 def get_dkg_broadcast_filter(web3, group_index):
     contract = get_dkg_contract(web3)
-    return contract.events.BroadcastAndKeyShare.createFilter(fromBlock = 653369, argument_filters={'groupIndex': group_index})
+    return contract.events.BroadcastAndKeyShare.createFilter(fromBlock = 0, argument_filters={'groupIndex': group_index})
 
 def get_dkg_complaint_sent_filter(web3, group_index, to_node_index):
     contract = get_dkg_contract(web3)
-    return contract.events.ComplaintSent.createFilter(fromBlock = 653369, argument_filters={'groupIndex': group_index, 'toNodeIndex': to_node_index})
+    return contract.events.ComplaintSent.createFilter(fromBlock = 0, argument_filters={'groupIndex': group_index, 'toNodeIndex': to_node_index})
 
 def get_dkg_all_complaints_filter(web3, group_index):
     contract = get_dkg_contract(web3)
-    return contract.events.ComplaintSent.createFilter(fromBlock = 653369, argument_filters={'groupIndex': group_index})
+    return contract.events.ComplaintSent.createFilter(fromBlock = 0, argument_filters={'groupIndex': group_index})
 
 def get_dkg_successful_filter(web3, group_index):
     contract = get_dkg_contract(web3)
-    return contract.events.SuccessfulDKG.createFilter(fromBlock = 653369, argument_filters={'groupIndex': group_index})
+    return contract.events.SuccessfulDKG.createFilter(fromBlock = 0, argument_filters={'groupIndex': group_index})
 
 def get_dkg_fail_filter(web3, group_index):
     contract = get_dkg_contract(web3)
-    return contract.events.FailedDKG.createFilter(fromBlock = 653369, argument_filters={'groupIndex': group_index})
+    return contract.events.FailedDKG.createFilter(fromBlock = 0, argument_filters={'groupIndex': group_index})
 
 def get_dkg_all_data_received_filter(web3, group_index):
     contract = get_dkg_contract(web3)
-    return contract.events.AllDataReceived.createFilter(fromBlock = 653369, argument_filters={'groupIndex': group_index})
+    return contract.events.AllDataReceived.createFilter(fromBlock = 0, argument_filters={'groupIndex': group_index})
 
 def get_dkg_bad_guy_filter(web3):
     contract = get_dkg_contract(web3)
-    return contract.events.BadGuy.createFilter(fromBlock = 653369)
+    return contract.events.BadGuy.createFilter(fromBlock = 0)
 
 def get_schains_data_contract(web3):
     custom_contracts_contracts_data = read_custom_contracts_data()
