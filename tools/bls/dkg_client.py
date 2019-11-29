643b17b9f70cb1de7041e73664a4af82b69145d5
Oleh Nikolaiev <oleg@skalelabs.com>
2019-11-29 17:17:34 +0200

    SKALE-1770 Fix generating keey

--- tools/bls/dkg_client.py
+++ tools/bls/dkg_client.py
@@ -66,8 +66,6 @@ class DKGClient:
         self.node_id_dkg = node_id_dkg
         self.skale = skale
         self.eth_key_name = eth_key_name
-        self.t = t
-        self.n = n
         self.incoming_verification_vector = ['0'] * n
         self.incoming_secret_key_contribution = ['0'] * n
         self.public_keys = public_keys
@@ -166,7 +164,7 @@ class DKGClient:
         logger.info(f'All data from {self.node_ids_contract[fromNode]} was recieved and verified')
 
     def GenerateKey(self, bls_key_name):
-        return self.sgx.create_bls_private_key(bls_key_name, self.eth_key_name, self.poly_name, "".join(secret_key_contribution[j][192*self.node_id_dkg:192*(self.node_id_dkg + 1)] for j in range(n)))
+        return self.sgx.create_bls_private_key(bls_key_name, self.eth_key_name, self.poly_name, "".join(secret_key_contribution[j][192*self.node_id_dkg:192*(self.node_id_dkg + 1)] for j in range(self.sgx.n)))
 
     def Allright(self):
         res = self.skale.dkg.allright(self.group_index, self.node_id_contract)
