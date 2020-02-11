33ac0642b78362f30d0d516ba4b741ee9df981ad
Dmytro Nazarenko <dmytro.nazarenko@nure.ua>
2020-02-11 15:38:10 +0200

    SKALE-2121 Add start_exit unit test

--- tests/node_test.py
+++ tests/node_test.py
@@ -3,7 +3,7 @@ import os
 import mock
 import pytest
 
-from core.node import Node
+from core.node import Node, NodeExitStatuses
 from core.node_config import NodeConfig
 
 DIR_PATH = os.path.dirname(os.path.realpath(__file__))
@@ -64,3 +64,17 @@ def test_register_info(node):
     assert info['port'] == int(port)
     assert info['id'] == node.config.id
     assert info['publicKey'] == node.skale.wallet.public_key
+
+
+def test_start_exit(node):
+    ip = '1.1.1.1'
+    public_ip = '2.2.2.2'
+    port = 8080
+
+    with mock.patch('core.node.run_filebeat_service'):
+        node.register(ip, public_ip, port, NODE_NAME)
+
+    node.exit({})
+    status = NodeExitStatuses(node.skale.nodes_data.get_node_status(node.config.id))
+
+    assert status != NodeExitStatuses.ACTIVE
