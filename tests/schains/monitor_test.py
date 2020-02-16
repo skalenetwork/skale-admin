7493c3a345d5c3f89469a9a80713d365dcaa1766
Dmytro Nazarenko <dmytro.nazarenko@nure.ua>
2020-02-16 18:20:27 +0200

    SKALE-2182 Add rotate_schain test

--- tests/schains/monitor_test.py
+++ tests/schains/monitor_test.py
@@ -4,12 +4,15 @@ from core.schains.monitor import SchainsMonitor, run_schain_container, run_ima_c
 from tools.docker_utils import DockerUtils
 from core.schains.runner import get_container_name, get_image_name
 from tools.configs.containers import SCHAIN_CONTAINER, IMA_CONTAINER
+import mock
 
 
 @pytest.fixture
 def monitor(skale):
     config = NodeConfig()
-    monitor = SchainsMonitor(skale, config)
+    with mock.patch('core.schains.monitor.SchainsMonitor.wait_for_node_id'):
+        monitor = SchainsMonitor(skale, config)
+        monitor.node_id = 1
     return monitor
 
 
@@ -37,6 +40,15 @@ def test_rotate_schain(monitor, dutils):
     schain_container_name = get_container_name(SCHAIN_CONTAINER, SCHAIN_NAME)
     ima_image = get_image_name(IMA_CONTAINER)
     schain_image = get_image_name(SCHAIN_CONTAINER)
-    schain_cont = dutils.client.containers.run(schain_image, name=schain_container_name, detach=True)
-    ima_cont = dutils.client.containers.run(ima_image, name=ima_container_name, detach=True)
-    monitor.rotate(SCHAIN)
+    dutils.client.containers.run(schain_image, name=schain_container_name, detach=True)
+    dutils.client.containers.run(ima_image, name=ima_container_name, detach=True)
+    schain_cont = dutils.client.containers.get(schain_container_name)
+    ima_cont = dutils.client.containers.get(ima_container_name)
+    with mock.patch('core.schains.monitor.generate_schain_config'), \
+            mock.patch('core.schains.monitor.save_schain_config'):
+            monitor.rotate_schain(SCHAIN)
+    restarted_schain = dutils.client.containers.get(schain_container_name)
+    restarted_ima = dutils.client.containers.get(ima_container_name)
+    assert schain_cont.attrs['State']['StartedAt'] != restarted_schain.attrs['State']['StartedAt']
+    assert ima_cont.attrs['State']['StartedAt'] != restarted_ima.attrs['State']['StartedAt']
+    monitor.scheduler.shutdown()
