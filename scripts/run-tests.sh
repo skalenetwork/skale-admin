e89f431519ddce485aa76c29fe14fcdf31df068f
Dmytro Tkachuk <dmitry.tkk@gmail.com>
2020-02-18 18:13:52 +0200

    SKALE-2198 Fix tests

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -3,16 +3,13 @@ rm tests/skale-data/node_data/node_config.json
 
 set -e
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-PROJECT_DIR=$(dirname $DIR)
-
 export SKALE_DIR_HOST=$PWD/tests/skale-data
 export RUNNING_ON_HOST=True
 export PYTHONPATH=${PYTHONPATH}:.
 export ENV=dev
 
-python $PROJECT_DIR/tests/prepare_data.py
+python tests/prepare_data.py
 
-py.test $PROJECT_DIR/tests/ --ignore=tests/firewall
+py.test tests/ --ignore=tests/firewall
 find . -name \*.pyc -delete
-$PROJECT_DIR/scripts/run_firewall_test.sh
+scripts/run_firewall_test.sh
