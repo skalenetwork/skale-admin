ea32af2595ce347eb7cb09aeb81ecaf68c78c634
Dmytro Tkachuk <dmitry.tkk@gmail.com>
2020-02-18 18:06:43 +0200

    SKALE-2198 Fix tests

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -2,11 +2,17 @@ docker rm -f skale_schain_test && docker volume rm test
 rm tests/skale-data/node_data/node_config.json
 
 set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+PROJECT_DIR=$(dirname $DIR)
+
 export SKALE_DIR_HOST=$PWD/tests/skale-data
 export RUNNING_ON_HOST=True
 export PYTHONPATH=${PYTHONPATH}:.
 export ENV=dev
 
-py.test tests/ --ignore=tests/firewall
+python $PROJECT_DIR/tests/prepare_data.py
+
+py.test $PROJECT_DIR/tests/ --ignore=tests/firewall
 find . -name \*.pyc -delete
-scripts/run_firewall_test.sh
+$PROJECT_DIR/scripts/run_firewall_test.sh
