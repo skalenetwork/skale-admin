72c507c5af84086a29b7b430ecedf7c9ea418fd7
Dmytro Tkachuk <dmitry.tkk@gmail.com>
2020-03-10 16:35:26 +0200

    SKALE-2198 Fix sgx simulator version

--- scripts/run_sgx_simulator.sh
+++ scripts/run_sgx_simulator.sh
@@ -3,4 +3,4 @@
 # set -e
 
 docker rm -f sgx_simulator || true
-docker run -d -p 1026-1028:1026-1028 --name sgx_simulator skalenetwork/sgxwalletsim:latest -s -y -a
+docker run -d -p 1026-1028:1026-1028 --name sgx_simulator skalenetwork/sgxwalletsim:latest_commit -s -y -a
