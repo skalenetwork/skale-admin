d0bd30746b422ad3a2d18c9bd0154685097cb480
Dmytro Nazarenko <dmytro.nazarenko@nure.ua>
2020-03-24 19:43:51 +0200

    SKALE-2326 Add create node test

--- tests/utils.py
+++ tests/utils.py
@@ -31,3 +31,8 @@ def generate_random_schain_data():
 def get_bp_data(bp, request, params=None):
     data = bp.get(request, query_string=params).data
     return json.loads(data.decode('utf-8'))['data']
+
+
+def post_bp_data(bp, request, params=None):
+    data = bp.post(request, json=params).data
+    return json.loads(data.decode('utf-8'))['data']
