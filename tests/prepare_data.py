3e779d2a6dfdb9cccd4091761a26bdbc9af443cf
ivd <ivan@skalelabs.com>
2020-04-28 15:42:26 +0300

    SKALE-2501 Remove redundant function from prepare_data.py

--- tests/prepare_data.py
+++ tests/prepare_data.py
@@ -4,7 +4,6 @@ import logging
 
 from skale.utils.contracts_provision.main import setup_validator, _skip_evm_time
 from skale.utils.contracts_provision import MONTH_IN_SECONDS
-from skale.wallets.web3_wallet import generate_wallet
 
 from tests.conftest import skale
 
@@ -24,26 +23,8 @@ def cleanup_contracts(skale):
         skale.manager.delete_node_by_root(node_id, wait_for=True)
 
 
-def link_node_address(skale):
-    validator_id = skale.validator_service.validator_id_by_address(
-        skale.wallet.address)
-    main_wallet = skale.wallet
-    wallet = generate_wallet(skale.web3)
-    skale.wallet = wallet
-    signature = skale.validator_service.get_link_node_signature(
-        validator_id=validator_id
-    )
-    skale.wallet = main_wallet
-    skale.validator_service.link_node_address(
-        node_address=wallet.address,
-        signature=signature,
-        wait_for=True
-    )
-
-
 if __name__ == "__main__":
     skale_lib = skale()
     cleanup_contracts(skale_lib)
     setup_validator(skale_lib)
-    link_node_address(skale_lib)
     _skip_evm_time(skale_lib.web3, MONTH_IN_SECONDS)
