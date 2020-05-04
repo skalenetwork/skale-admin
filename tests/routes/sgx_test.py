c10a44fc0acf9af902572ccbeff8649aabfc4c72
ivd <ivan@skalelabs.com>
2020-05-04 14:55:36 +0300

    SKALE-2320 Fix sgx_test

--- tests/routes/sgx_test.py
+++ tests/routes/sgx_test.py
@@ -18,7 +18,7 @@ def test_sgx_status(skale_bp, skale):
     assert data == {
         'payload': {
             'sgx_server_url': SGX_SERVER_URL,
-            'status': 1,
+            'status': 0,
             'status_name': 'NOT_CONNECTED'
         },
         'status': 'ok'
