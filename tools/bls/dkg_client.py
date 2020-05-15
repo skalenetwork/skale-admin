4e5cc4c937492d0195e3ab7cd9d293c7136f6be1
Oleh <oleg@skalelabs.com>
2020-05-16 00:09:50 +0300

    SKALE-2523 alright possible

--- tools/bls/dkg_client.py
+++ tools/bls/dkg_client.py
@@ -260,9 +260,13 @@ class DKGClient:
         is_alright_possible = is_alright_possible_function(
             self.group_index, self.node_id_contract).call({'from': self.skale.wallet.address})
 
-        if not is_alright_possible or not self.is_channel_opened():
+        if not is_alright_possible:
             logger.info(f'sChain: {self.schain_name}. '
                         f'{self.node_id_dkg} node has already sent an alright note')
+            return
+
+        if not self.is_channel_opened():
+            logger.info(f'sChain: {self.schain_name}. channel is not opened')
             return
         try:
             tx_res = self.skale.dkg.alright(
