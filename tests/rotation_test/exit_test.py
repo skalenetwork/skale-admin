50c61c3b3b97886ace5b4eb029b37a7e9b200d66
Dmytro Nazarenko <dmytro.nazarenko@nure.ua>
2020-06-11 13:54:43 +0300

    SKALE-2266 Make node exit in unit tests

--- tests/rotation_test/exit_test.py
+++ tests/rotation_test/exit_test.py
@@ -1,19 +1,25 @@
+from time import sleep
 from unittest import mock
 
 import pytest
 import json
+
+from skale.manager_client import spawn_skale_lib
 
 from core.node import Node
 from core.node_config import NodeConfig
 from core.schains.creator import monitor
 from core.schains.runner import run_schain_container
 from core.schains.volume import init_data_volume
-from tests.utils import generate_random_node_data
+from tests.dkg_test.main_test import run_dkg_all, generate_sgx_wallets, transfer_eth_to_wallets, \
+    link_addresses_to_validator, register_nodes
+from tests.utils import generate_random_name
 from tools.bls.dkg_utils import get_secret_key_share_filepath
 from tools.docker_utils import DockerUtils
 from web.models.schain import SChainRecord
 
 dutils = DockerUtils(volume_driver='local')
+
 
 SECRET_KEY_INFO = {
     "common_public_key": [
@@ -46,31 +52,32 @@ def run_schain_container_mock(schain, env):
     return run_schain_container(schain, env, dutils)
 
 
+def set_up_nodes(skale, nodes_number):
+    wallets = generate_sgx_wallets(skale, nodes_number)
+    transfer_eth_to_wallets(skale, wallets)
+    link_addresses_to_validator(skale, wallets)
+    nodes_data = register_nodes(skale, wallets)
+    return nodes_data
+
+
 @pytest.fixture
 def exiting_node(skale):
-    name = 'exit_test'
-    ip, public_ip, port, _ = generate_random_node_data()
-    skale.manager.create_node(ip, port, name, public_ip, wait_for=True)
+    nodes = set_up_nodes(skale, 2)
     config = NodeConfig()
-    config.id = skale.nodes_data.node_name_to_index(name)
+    config.id = nodes[0]['node_id']
 
-    name = f'rotation_test_0'
-    ip, public_ip, port, _ = generate_random_node_data()
-    skale.manager.create_node(ip, port, name, public_ip, wait_for=True)
-
-    schain_name = 'exit_schain'
+    schain_name = generate_random_name()
     skale.manager.create_default_schain(schain_name)
 
-    name = f'rotation_test_1'
-    ip, public_ip, port, _ = generate_random_node_data()
-    skale.manager.create_node(ip, port, name, public_ip, wait_for=True)
+    run_dkg_all(skale, schain_name, nodes)
+    nodes.append(set_up_nodes(skale, 1)[0])
 
-    yield Node(skale, config)
+    exit_skale_lib = spawn_skale_lib(skale)
+    exit_skale_lib.wallet = nodes[0]['wallet']
+    yield Node(exit_skale_lib, config)
     skale.manager.delete_schain(schain_name, wait_for=True)
-    for i in range(2):
-        name = f'rotation_test_{i}'
-        id = skale.nodes_data.node_name_to_index(name)
-        skale.manager.delete_node_by_root(id, wait_for=True)
+    for i in range(1, 3):
+        skale.manager.delete_node_by_root(nodes[i]['node_id'], wait_for=True)
 
 
 def test_node_exit(skale, exiting_node):
@@ -82,3 +89,7 @@ def test_node_exit(skale, exiting_node):
             mock.patch('core.schains.checks.apsent_iptables_rules',
                        new=mock.Mock(return_value=[True, True])):
         monitor(skale, exiting_node.config)
+        exiting_node.exit({})
+        while skale.nodes_data.get_node_status(exiting_node.config.id) != 2:
+            sleep(10)
+        print(exiting_node.get_exit_status())
