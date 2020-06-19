6fc6820bc676fe674067f9ebee05b978bd4658da
Dmytro Nazarenko <dmytro.nazarenko@nure.ua>
2020-06-19 12:32:24 +0300

    SKALE-2749 Add snapshotInterval

--- tests/rotation_test/five_nodes/utils.py
+++ tests/rotation_test/five_nodes/utils.py
@@ -22,6 +22,8 @@ def get_args(node_id):
     node_base_port = node_id*base_port
     opts = (
         f'--config /schain_data/config.json '
+        f'-d /schain_data/data_dir '
+        f'--ipcpath /schain_data/data_dir '
         f'--http-port {node_base_port + SkaledPorts.HTTP_JSON.value} '
         f'--https-port {node_base_port + SkaledPorts.HTTPS_JSON.value} '
         f'--ws-port {node_base_port + SkaledPorts.WS_JSON.value} '
@@ -35,22 +37,22 @@ def get_args(node_id):
     run_args['network_mode'] = 'host'
     run_args['environment'] = {
         'OPTIONS': opts,
-        'DATA_DIR': '/ schain_data/data_dir',
+        'DATA_DIR': '/schain_data/data_dir',
         'NO_NTP_CHECK': 1
     }
     run_args['volumes'] = {
         volume_host_path: {
             'bind': '/schain_data',
-            "mode": "ro"
+            "mode": "rw"
         }
     }
     return run_args
 
 
-def run_schain_containers():
+def run_schain_containers(nodes_count=4):
     image = get_image_name(SCHAIN_CONTAINER)
 
-    for i in range(1, 5):
+    for i in range(1, nodes_count + 1):
         run_args = get_args(i)
         dutils.client.containers.run(image, name=f'node{i}', detach=True, **run_args)
 
