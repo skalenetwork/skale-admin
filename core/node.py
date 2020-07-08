76e2a7b6b272e193522f6146cb269c67191953e8
Dmytro <dmitry.tkk@gmail.com>
2020-07-08 17:38:56 +0300

    SKALE-2747 Add support for MONITORING_CONTAINERS option

--- core/node.py
+++ core/node.py
@@ -21,15 +21,15 @@ import logging
 import time
 from enum import Enum
 
-
-from tools.str_formatters import arguments_list_string
-from tools.wallet_utils import check_required_balance
-
 from skale.transactions.result import TransactionFailedError
 from skale.utils.helper import ip_from_bytes
 from skale.wallets.web3_wallet import public_key_to_address
 
 from core.filebeat import run_filebeat_service
+
+from tools.str_formatters import arguments_list_string
+from tools.wallet_utils import check_required_balance
+from tools.configs.filebeat import MONITORING_CONTAINERS
 
 logger = logging.getLogger(__name__)
 
@@ -90,7 +90,8 @@ class Node:
 
         self._log_node_info('Node successfully created', ip, public_ip, port, name)
         self.config.id = self.skale.nodes.node_name_to_index(name)
-        run_filebeat_service(public_ip, self.config.id, self.skale)
+        if MONITORING_CONTAINERS:
+            run_filebeat_service(public_ip, self.config.id, self.skale)
         return {'status': 1, 'data': self.config.all()}
 
     def exit(self, opts):
