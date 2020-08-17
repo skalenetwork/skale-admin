1f269ffd42a65917cb3b363f5e369b147b47df22
badrogger <ivan@skalelabs.com>
2020-08-17 17:58:57 +0300

    Add additional logs

--- core/schains/creator.py
+++ core/schains/creator.py
@@ -90,6 +90,8 @@ def monitor(skale, node_config):
     node_info = node_config.all()
     notify_if_not_enough_balance(skale, node_info)
 
+    logger.info('Starting schains ThreadPoolExecutor')
+
     with ThreadPoolExecutor(max_workers=max(1, schains_on_node)) as executor:
         futures = [
             executor.submit(
@@ -107,6 +109,7 @@ def monitor(skale, node_config):
 
 
 def monitor_schain(skale, node_info, schain, ecdsa_sgx_key_name):
+    logger.info("Monitor for sChain {schain['name']}")
     skale = spawn_skale_lib(skale)
     name = schain['name']
     node_id, sgx_key_name = node_info['node_id'], node_info['sgx_key_name']
