24dec7301e342544ae627433f97334c172d42d1a
Oleh <oleg@skalelabs.com>
2020-08-19 19:09:05 +0300

    SKALE-3024 fix flake

--- tools/bls/dkg_client.py
+++ tools/bls/dkg_client.py
@@ -306,7 +306,7 @@ class DKGClient:
     def get_complaint_data(self):
         get_complaint_data_function = self.dkg_contract_functions.getComplaintData
         return get_complaint_data_function(self.group_index).call()
-    
+
     def get_time_of_last_successful_dkg(self):
         time_of_last_successful_dkg_function = self.dkg_contract_functions.getTimeOfLastSuccesfulDKG
         return time_of_last_successful_dkg_function(self.group_index).call()
