957d6913bf8996f86596a41b3c2c13f882339747
Dmytro <dmitry.tkk@gmail.com>
2020-10-16 16:09:41 +0300

    SKALE-3355 Modify migrations procedure

--- web/migrations.py
+++ web/migrations.py
@@ -18,19 +18,39 @@
 #   along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 # This file will include all migrations for the SQLite database
+# To add a new field create a new method named `add_FIELD_NAME_field` to this file and invoke it
+# the `run_migrations` method
+
+import logging
 
 from playhouse.migrate import SqliteMigrator, migrate
 from peewee import BooleanField
 from tools.db import get_database
 
 
+logger = logging.getLogger(__name__)
+
+
 def run_migrations():
-    db = get_database
+    logging.info('Running migrations...')
+    db = get_database()
     migrator = SqliteMigrator(db)
-    add_new_schain_field(migrator)
+    add_new_schain_field(db, migrator)
 
 
-def add_new_schain_field(migrator):
-    migrate(
-        migrator.add_column('SChainRecord', 'new_schain', BooleanField(default=True))
-    )
+def add_new_schain_field(db, migrator):
+    add_column(db, migrator, 'SChainRecord', 'new_schain', BooleanField(default=True))
+
+
+def find_column(db, table_name, column_name):
+    columns = db.get_columns(table_name)
+    return next((x for x in columns if x.name == column_name), None)
+
+
+def add_column(db, migrator, table_name, column_name, field):
+    logging.info(f'Add column: {table_name}.{column_name}')
+    if not find_column(db, table_name, column_name):
+        logging.info(f'Going to add: {table_name}.{column_name}')
+        migrate(
+            migrator.add_column(table_name, column_name, field)
+        )
