ca28703537e19dee6b374e248b82d5c3ead60f44
Dmytro <dmitry.tkk@gmail.com>
2020-12-02 16:36:58 +0200

    SKALE-3580 Enable core tests

---
+++ scripts/run_core_tests.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -e
+
+export DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+source $DIR/helper.sh
+source $DIR/../helper-scripts/helper.sh
+
+: "${SGX_WALLET_TAG?Need to set SGX_WALLET_TAG}"
+
+tests_cleanup
+export_test_env
+
+run_sgx_simulator $SGX_WALLET_TAG
+bash scripts/run_redis.sh
+
+python tests/prepare_data.py
+scripts/run_firewall_test.sh
+
+tests_cleanup
