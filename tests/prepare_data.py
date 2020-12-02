f774c420195815f8468a49bc1fd93acef8501145
Dmytro <dmitry.tkk@gmail.com>
2020-12-02 19:03:16 +0200

    SKALE-3580 Add logger to prepare_data

--- tests/prepare_data.py
+++ tests/prepare_data.py
@@ -2,6 +2,7 @@
 
 import logging
 
+from skale.utils.helper import init_default_logger
 from skale.utils.contracts_provision.main import (
     setup_validator, _skip_evm_time, create_nodes, cleanup_nodes_schains
 )
@@ -13,6 +14,7 @@ logger = logging.getLogger(__name__)
 
 
 if __name__ == "__main__":
+    init_default_logger()
     skale = init_skale()
     cleanup_nodes_schains(skale)
     setup_validator(skale)
