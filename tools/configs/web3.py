8992098b14367d50dcfaa491ef27ea54c009427d
badrogger <ivan@skalelabs.com>
2021-01-21 17:10:21 +0200

    SKALE-3753 Save state in a separate file

--- tools/configs/web3.py
+++ tools/configs/web3.py
@@ -25,7 +25,8 @@ TM_URL = os.environ['TM_URL']
 ENDPOINT = os.environ['ENDPOINT']
 ABI_FILEPATH = os.path.join(CONTRACTS_INFO_FOLDER, MANAGER_CONTRACTS_INFO_NAME)
 STATE_FILENAME = os.getenv('STATE_FILENAME')
+STATE_BASE_PATH = os.path.join(NODE_DATA_PATH, 'eth-state')
 STATE_FILEPATH = None if not STATE_FILENAME \
-                    else os.path.join(NODE_DATA_PATH, STATE_FILENAME)
+                    else os.path.join(STATE_BASE_PATH, STATE_FILENAME)
 
 NODE_REGISTER_CONFIRMATION_BLOCKS = 5
