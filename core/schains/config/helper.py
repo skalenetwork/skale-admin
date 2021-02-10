1c03768f292d123dac6d177acead5160cddc344c
Dmytro <dmitry.tkk@gmail.com>
2021-02-10 16:13:42 +0200

    SKALE-3857 Fix sChain config helper

--- core/schains/config/helper.py
+++ core/schains/config/helper.py
@@ -261,7 +261,7 @@ def get_schain_container_base_opts(schain_name: str,
     ports = get_schain_ports(schain_name)
 
     static_schain_params = get_static_schain_params()
-    static_schain_cmd = static_schain_params['schain_cmd']
+    static_schain_cmd = static_schain_params.get('schain_cmd', None)
     cmd = [
         f'--config {config_filepath}',
         f'-d {DATA_DIR_CONTAINER_PATH}',
@@ -270,7 +270,11 @@ def get_schain_container_base_opts(schain_name: str,
         f'--https-port {ports["https"]}',
         f'--ws-port {ports["ws"]}',
         f'--wss-port {ports["wss"]}'
-    ].extend(static_schain_cmd)
+    ]
+
+    if static_schain_cmd:
+        cmd.extend(static_schain_cmd)
+
     if enable_ssl:
         cmd.extend([
             f'--ssl-key {ssl_key}',
