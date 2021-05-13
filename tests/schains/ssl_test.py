1db06b68c55ac04d0def4ee9146561f6ab3f998b
badrogger <ivan@skalelabs.com>
2021-05-13 17:20:41 +0300

    SKALE-4104 Fix schains ssl test

--- tests/schains/ssl_test.py
+++ tests/schains/ssl_test.py
@@ -4,7 +4,7 @@ from tools.configs import NODE_DATA_PATH
 from core.schains.ssl import get_ssl_filepath
 
 
-def test_get_ssl_filepath():
+def test_get_ssl_filepath(cert_key_pair):
     ssl_key_path, ssl_cert_path = get_ssl_filepath()
 
     certs_filepath = os.path.join(NODE_DATA_PATH, 'ssl')
