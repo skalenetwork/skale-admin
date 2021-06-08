7a5b239546018e2cdd46034e708350fb14c58f0c
Dmytro <dmitry.tkk@gmail.com>
2021-06-08 15:38:59 +0300

    SKALE-4252 Add 1155 token to IMA connect sChains

--- tests/schains/creator_test.py
+++ tests/schains/creator_test.py
@@ -348,7 +348,8 @@ def test_monitor_ima(skale_ima, schain_on_contracts, schain_config, dutils):
             schain_ima_abi['community_locker_address'],
             schain_ima_abi['token_manager_eth_address'],
             schain_ima_abi['token_manager_erc20_address'],
-            schain_ima_abi['token_manager_erc721_address']
+            schain_ima_abi['token_manager_erc721_address'],
+            schain_ima_abi['token_manager_erc1155_address']
         ]
     )
     with mock.patch('core.schains.monitor.copy_schain_ima_abi', return_value=True):
