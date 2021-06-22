209ec03c73ad0c782c1a5996330448df24637c67
Dmytro Nazarenko <dmytro.nazarenko@nure.ua>
2021-06-22 13:48:59 +0300

    SKALE-4330 Change nginx conf path

--- tools/configs/nginx.py
+++ tools/configs/nginx.py
@@ -1,7 +1,7 @@
 import os
 
-from tools.configs import CONFIG_FOLDER
+from tools.configs import CONFIG_FOLDER, NODE_DATA_PATH
 
 NGINX_CONTAINER_NAME = 'skale_nginx'
 NGINX_TEMPLATE_FILEPATH = os.path.join(CONFIG_FOLDER, 'nginx.conf.j2')
-NGINX_CONFIG_FILEPATH = os.path.join(CONFIG_FOLDER, 'nginx.conf')
+NGINX_CONFIG_FILEPATH = os.path.join(NODE_DATA_PATH, 'nginx.conf')
