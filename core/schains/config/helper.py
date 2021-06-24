c58d0c6b1ea2c67d745d4211b7ad90fa2ff30503
Dmytro <dmitry.tkk@gmail.com>
2021-06-24 18:16:26 +0300

    SKALE-4338 Fix ima state path

--- core/schains/config/helper.py
+++ core/schains/config/helper.py
@@ -220,8 +220,7 @@ def get_schain_env(ulimit_check=True):
     env = {'SEGFAULT_SIGNALS': 'all'}
     if not ulimit_check:
         env.update({
-            'NO_ULIMIT_CHECK': 1,
-            'NO_NTP_CHECK': 1
+            'NO_ULIMIT_CHECK': 1
         })
     return env
 
