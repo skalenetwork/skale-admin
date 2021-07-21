2f5941002c26a89b089b5ac83cd326071fe44eb6
badrogger <ivan@skalelabs.com>
2021-07-21 17:45:23 +0300

    Add process to skale_api logs

--- tools/configs/logs.py
+++ tools/configs/logs.py
@@ -44,4 +44,4 @@ LOG_FILE_SIZE_BYTES = LOG_FILE_SIZE_MB * 1000000
 LOG_BACKUP_COUNT = 3
 
 ADMIN_LOG_FORMAT = '[%(asctime)s %(levelname)s] - %(process)d - %(threadName)s - %(name)s:%(lineno)d - %(message)s'  # noqa
-API_LOG_FORMAT = '[%(asctime)s] %(levelname)s %(url)s %(module)s: %(message)s'  # noqa
+API_LOG_FORMAT = '[%(asctime)s] %(process)d %(levelname)s %(url)s %(module)s: %(message)s'  # noqa
