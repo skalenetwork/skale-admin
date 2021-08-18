7adb2830a2d0629dd7fbdc08bd21d8730f0927b4
badrogger <ivan@skalelabs.com>
2021-08-18 12:21:02 +0300

    Fix rotation tests

--- tests/rotation_test/new_test.py
+++ tests/rotation_test/new_test.py
@@ -4,7 +4,7 @@ from unittest import mock
 import pytest
 import os
 
-from core.schains.monitor import run_monitor_for_schain
+from core.schains.monitor import run_monitor_for_schain, monitor_schain
 from core.schains.checks import SChainChecks
 from skale.utils.contracts_provision.main import cleanup_nodes_schains
 from tests.rotation_test.utils import (
@@ -33,7 +33,7 @@ def rotated_nodes(skale, schain_config, schain_db, cert_key_pair, dutils):
         skale.manager.node_exit(nodes[i].config.id, wait_for=True)
 
 
-def test_new_node(skale, skale_ima, rotated_nodes):
+def test_new_node(skale, skale_ima, rotated_nodes, dutils):
     nodes, schain_name = rotated_nodes
 
     exited_node, new_node = nodes[0], nodes[2]
@@ -45,7 +45,7 @@ def test_new_node(skale, skale_ima, rotated_nodes):
             mock.patch(
                 'core.schains.monitor.init_data_volume',
                 init_data_volume_mock
-            ), \
+    ), \
             mock.patch('core.schains.monitor.run_dkg', run_dkg_mock), \
             mock.patch('core.schains.checks.apsent_iptables_rules',
                        new=mock.Mock(return_value=[True, True])):
@@ -54,15 +54,19 @@ def test_new_node(skale, skale_ima, rotated_nodes):
         node_info = new_node.config.all()
         schain = skale.schains.get_by_name(schain_name)
 
-        run_monitor_for_schain(
+        monitor_schain(
             new_node.skale,
             skale_ima,
             node_info=node_info,
             schain=schain,
             ecdsa_sgx_key_name=ecdsa_sgx_key_name,
-            loop=False
+            dutils=dutils
         )
-        checks = SChainChecks(schain_name, new_node.config.id).get_all()
+        checks = SChainChecks(
+            schain_name,
+            new_node.config.id,
+            dutils=dutils
+        ).get_all()
         assert checks['container']
         assert checks['volume']
         assert checks['data_dir']
