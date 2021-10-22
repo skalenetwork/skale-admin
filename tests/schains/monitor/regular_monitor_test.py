01f798a44a96f9bd76aae39d6f1849363ae4f741
Dmytro <dmitry.tkk@gmail.com>
2021-10-22 19:15:34 +0300

    SKALE-4602 Update regular monitor test

--- tests/schains/monitor/regular_monitor_test.py
+++ tests/schains/monitor/regular_monitor_test.py
@@ -1,10 +1,26 @@
+import logging
+
 import pytest
 from core.schains.monitor import RegularMonitor
+
+logger = logging.getLogger(__name__)
+
+
+def add_firewall_rules_mock(name):
+    pass
+
+
+class TestRegularMonitor(RegularMonitor):
+    def firewall_rules(self, overwrite) -> None:
+        if not self.checks.firewall_rules.status or overwrite:
+            add_firewall_rules_mock(self.name)
+        else:
+            logger.info(f'{self.p} firewall_rules - ok')
 
 
 def test_regular_monitor(schain_db, skale, node_config, schain_checks, schain_on_contracts):
     # todo: use existing chain here!
-    test_monitor = RegularMonitor(
+    test_monitor = TestRegularMonitor(
         skale=skale,
         schain=schain_on_contracts,
         node_config=node_config,
@@ -22,7 +38,7 @@ def test_regular_monitor_new_chain(schain_db, skale, node_config, schain_checks,
     # todo: emulate dkg for the second node
     # todo: run monitor for the first node
 
-    test_monitor = RegularMonitor(
+    test_monitor = TestRegularMonitor(
         skale=skale,
         schain=schain_on_contracts,
         node_config=node_config,
