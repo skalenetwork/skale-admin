29748e90876a8392034382b202d64b471570c3dd
badrogger <ivan@skalelabs.com>
2021-11-09 17:22:24 +0200

    SKALE-4603 Remove debug output

--- scripts/run_core_tests.sh
+++ scripts/run_core_tests.sh
@@ -13,11 +13,6 @@ export_test_env
 run_sgx_simulator $SGX_WALLET_TAG
 bash scripts/run_redis.sh
 
-docker ps -a
-docker logs ganache --tail 200
-sleep 500
-curl http://127.0.0.1:8545
-
 python tests/prepare_data.py
 py.test --cov-config=.coveragerc --cov=. tests/ --ignore=tests/firewall --ignore=tests/rotation_test $@
 tests_cleanup
