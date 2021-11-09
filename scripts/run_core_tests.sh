8b9d44ee233f379f306595e3bcc4147027f4baaf
badrogger <ivan@skalelabs.com>
2021-11-09 16:14:30 +0200

    SKALE-4603 Add debug info to run_core_tests script

--- scripts/run_core_tests.sh
+++ scripts/run_core_tests.sh
@@ -13,6 +13,10 @@ export_test_env
 run_sgx_simulator $SGX_WALLET_TAG
 bash scripts/run_redis.sh
 
+docker ps -a
+docker logs ganache --tail 200
+curl http://127.0.0.1:8545
+
 python tests/prepare_data.py
 py.test --cov-config=.coveragerc --cov=. tests/ --ignore=tests/firewall --ignore=tests/rotation_test $@
 tests_cleanup
