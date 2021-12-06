993129f0703ddcc969a4d3fab2e45a0ad35f59a8
badrogger <ivan@skalelabs.com>
2021-12-06 17:27:38 +0000

    SKALE-4764 Fix regular_monitor_test

--- tests/schains/monitor/regular_monitor_test.py
+++ tests/schains/monitor/regular_monitor_test.py
@@ -10,7 +10,6 @@ from skale.utils.helper import ip_from_bytes
 from core.schains.checks import SChainChecks
 from core.schains.monitor import RegularMonitor
 from core.schains.ima import ImaData
-from core.schains.runner import get_container_name
 
 
 from tools.configs import (
@@ -21,18 +20,27 @@ from tools.configs import (
 from web.models.schain import SChainRecord
 
 from tests.dkg_utils import safe_run_dkg_mock
-from tests.utils import alter_schain_config
+from tests.utils import alter_schain_config, get_test_rule_controller
 
 
 logger = logging.getLogger(__name__)
 
 
-def test_regular_monitor(schain_db, skale, node_config, skale_ima, dutils, ssl_folder,
-                         rule_controller,
-                         schain_on_contracts):
+def test_regular_monitor(
+    schain_db,
+    skale,
+    node_config,
+    skale_ima,
+    dutils,
+    ssl_folder,
+    schain_on_contracts
+):
     schain_name = schain_on_contracts
     schain = skale.schains.get_by_name(schain_name)
     nodes = get_nodes_for_schain(skale, schain_name)
+
+    # not using rule_controller fixture to avoid config generation
+    rc = get_test_rule_controller(name=schain_name)
 
     sgx_wallet = SgxWallet(
         web3=skale.web3,
@@ -44,13 +52,12 @@ def test_regular_monitor(schain_db, skale, node_config, skale_ima, dutils, ssl_f
     node_config.ip = ip_from_bytes(nodes[0]['ip'])
     node_config.sgx_key_name = sgx_wallet.key_name
 
-    rule_controller.sync()
     schain_record = SChainRecord.get_by_name(schain_name)
     schain_checks = SChainChecks(
         schain_name,
         node_config.id,
         schain_record=schain_record,
-        rule_controller=rule_controller,
+        rule_controller=rc,
         dutils=dutils
     )
     ima_data = ImaData(False, '0x1')
@@ -61,19 +68,13 @@ def test_regular_monitor(schain_db, skale, node_config, skale_ima, dutils, ssl_f
         node_config=node_config,
         rotation_data={'rotation_id': 0},
         checks=schain_checks,
-        rule_controller=rule_controller,
+        rule_controller=rc,
         dutils=dutils
     )
 
     with mock.patch('core.schains.monitor.base_monitor.safe_run_dkg', safe_run_dkg_mock):
         test_monitor.run()
 
-    container = dutils.safe_get_container(
-        get_container_name('schain', schain_name)
-    )
-    tail_lines = container.logs(tail=300)
-    print(tail_lines)
-    # print(b''.join(tail_lines))
     assert schain_checks.config_dir.status
     assert schain_checks.dkg.status
     assert schain_checks.config.status
