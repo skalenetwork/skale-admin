1e689a2eafeb7fc199bda84442010e5afdc1bddf
badrogger <ivan@skalelabs.com>
2021-12-31 18:16:31 +0000

    SKALE-4793 Add SSLReloadMonitor

--- core/schains/monitor/ssl_reload_monitor.py
+++ core/schains/monitor/ssl_reload_monitor.py
@@ -17,10 +17,28 @@
 #   You should have received a copy of the GNU Affero General Public License
 #   along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-from .base_monitor import BaseMonitor  # noqa
-from .regular_monitor import RegularMonitor  # noqa
-from .repair_monitor import RepairMonitor  # noqa
-from .backup_monitor import BackupMonitor  # noqa
-from .rotation_monitor import RotationMonitor  # noqa
-from .post_rotation_monitor import PostRotationMonitor  # noqa
-from .ssl_reload_monitor import SSLReloadMonitor  # noqa
+import logging
+
+from core.schains.monitor import BaseMonitor, RegularMonitor
+from core.schains.runner import restart_container
+from tools.configs.containers import SCHAIN_CONTAINER
+
+logger = logging.getLogger(__name__)
+
+
+class SSLReloadMonitor(RegularMonitor):
+    """
+    SSLReloadMonitor is executed when new ssl certificates were uploaded
+    """
+    @BaseMonitor.monitor_runner
+    def run(self):
+        logger.info(
+            '%s. Restart requested. Going to restart sChain container',
+            self.p
+        )
+        restart_container(SCHAIN_CONTAINER, self.schain)
+        record = self.schain_record
+        record.set_restart_count(0)
+        record.set_failed_rpc_count(0)
+        record.set_needs_reload(False)
+        super().run()
