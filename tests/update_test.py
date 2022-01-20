8c44bff0bb5e2eeaa0b356db3ece3ca3b08e70e9
badrogger <ivan@skalelabs.com>
2022-01-20 15:18:15 +0000

    SKALE-4661 Improve update_node_config test

--- tests/update_test.py
+++ tests/update_test.py
@@ -3,9 +3,14 @@ from core.node_config import NodeConfig
 from core.updates import update_node_config_file
 
 
-NODE_STRUCT = {
+OLD_NODE_STRUCT = {
     'name': 'test',
     'ip': b'A\x80\x17\xe4'
+}
+
+NEW_NODE_STRUCT = {
+    'name': 'new-test',
+    'ip': b'\x01\x01\x01\x01'
 }
 
 
@@ -20,8 +25,15 @@ def test_update_node_config_file(skale):
     assert config.ip is None
 
     with mock.patch('skale.contracts.manager.nodes.Nodes.get',
-                    new=mock.Mock(return_value=NODE_STRUCT)):
+                    new=mock.Mock(return_value=OLD_NODE_STRUCT)):
         update_node_config_file(skale, config)
 
     assert config.name == 'test'
     assert config.ip == '65.128.23.228'
+
+    with mock.patch('skale.contracts.manager.nodes.Nodes.get',
+                    new=mock.Mock(return_value=NEW_NODE_STRUCT)):
+        update_node_config_file(skale, config)
+
+    assert config.name == 'new-test'
+    assert config.ip == '1.1.1.1'
