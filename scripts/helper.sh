c4a550cff231ea60b01399b12e986e9be3bb13a6
badrogger <ivan@skalelabs.com>
2022-07-28 15:35:18 +0000

    Add privileged cleanup for tests/skale-data

--- scripts/helper.sh
+++ scripts/helper.sh
@@ -30,10 +30,10 @@ export_test_env () {
 tests_cleanup () {
     export_test_env
     docker rm -f skale_schain_test && docker volume rm test || true
+    sudo rm -r tests/skale-data/
     rm tests/skale-data/node_data/node_config.json || true
     docker rm -f sgx-simulator || true
     docker rm -f skale_schain_test1 skale_schain_test2 skale_schain_test3 || true
     find . -name \*.pyc -delete || true
-    mkdir -p $SGX_CERTIFICATES_FOLDER || true
     rm -rf $SGX_CERTIFICATES_FOLDER/sgx.* || true
 }
