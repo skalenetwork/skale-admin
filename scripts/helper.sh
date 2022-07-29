e6f19e509343fc990cc1cf5f8cb5be37a1ef5c61
badrogger <ivan@skalelabs.com>
2022-07-29 15:51:08 +0000

    Fix tests

--- scripts/helper.sh
+++ scripts/helper.sh
@@ -35,5 +35,6 @@ tests_cleanup () {
     docker rm -f sgx-simulator || true
     docker rm -f skale_schain_test1 skale_schain_test2 skale_schain_test3 || true
     find . -name \*.pyc -delete || true
+    mkdir -p $SGX_CERTIFICATES_FOLDER || true
     rm -rf $SGX_CERTIFICATES_FOLDER/sgx.* || true
 }
