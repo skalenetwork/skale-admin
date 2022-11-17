fa704e95a9669594f7d384241360ac6036297613
badrogger <ivan@skalelabs.com>
2022-11-17 19:54:45 +0000

    Fix node_test

--- tests/node_test.py
+++ tests/node_test.py
@@ -3,10 +3,13 @@ import os
 import mock
 import pytest
 
+from skale import SkaleManager
 from skale.transactions.result import RevertError
-from skale.utils.account_tools import generate_account
+from skale.utils.account_tools import generate_account, send_eth
 from skale.utils.contracts_provision.main import generate_random_node_data
 from skale.utils.contracts_provision import DEFAULT_DOMAIN_NAME
+from skale.utils.helper import ip_from_bytes
+from skale.wallets import Web3Wallet
 
 from core.node import (
     get_block_device_size,
@@ -15,40 +18,87 @@ from core.node import (
 )
 from core.node_config import NodeConfig
 from tools.configs import NODE_DATA_PATH
+from tools.configs.web3 import ABI_FILEPATH
+from skale.utils.contracts_provision.main import (
+    cleanup_nodes,
+    link_nodes_to_validator,
+)
+from tests.utils import ENDPOINT, ETH_AMOUNT_PER_NODE
 
 DIR_PATH = os.path.dirname(os.path.realpath(__file__))
 
 
 @pytest.fixture
-def node(skale):
+def node(node_skales, skale, nodes):
     config = NodeConfig()
-    yield Node(skale, config)
+    node_index = 0
+    config.id = nodes[node_index]
+    node_data = skale.nodes.get(config.id)
+    config.name = node_data['name']
+    config.ip = ip_from_bytes(node_data['ip'])
+    yield Node(node_skales[0], config)
 
 
-def test_info_unregisted_node(node):
-    assert node.info == {'status': 5}
+@pytest.fixture
+def new_node_wallet(skale):
+    acc = generate_account(skale.web3)
+    pk = acc['private_key']
+    wallet = Web3Wallet(pk, skale.web3)
+    send_eth(
+        web3=skale.web3,
+        wallet=skale.wallet,
+        receiver_address=wallet.address,
+        amount=ETH_AMOUNT_PER_NODE
+    )
+    return wallet
 
 
-def test_create_insufficient_funds(node):
+@pytest.fixture
+def new_node_skale(skale, new_node_wallet):
+    return SkaleManager(ENDPOINT, ABI_FILEPATH, new_node_wallet)
+
+
+@pytest.fixture
+def unregistered_node(skale, new_node_skale, validator):
+    link_nodes_to_validator(skale, validator, [new_node_skale])
+    config = NodeConfig()
+    try:
+        node = Node(new_node_skale, config)
+        yield node
+    finally:
+        if node.config.id:
+            cleanup_nodes(skale, [node.config.id])
+
+
+def test_info_unregisted_node(unregistered_node):
+    assert unregistered_node.info == {'status': 5}
+
+
+def test_create_insufficient_funds(unregistered_node):
     ip = '1.1.1.2'
     public_ip = '2.2.2.3'
     port = 8081
     name = 'test-insuff'
     with mock.patch('core.node.check_required_balance',
                     new=mock.Mock(return_value=False)):
-        res = node.register(
-            ip, public_ip, port, name, domain_name=DEFAULT_DOMAIN_NAME)
+        res = unregistered_node.register(
+            ip,
+            public_ip,
+            port,
+            name,
+            domain_name=DEFAULT_DOMAIN_NAME
+        )
         assert res['status'] == 'error'
         assert res['errors'] == ['Insufficient funds, re-check your wallet']
 
 
-def test_register_info(node):
-    node.config.id = None
+def test_register_info(unregistered_node):
+    unregistered_node.config.id = None
     ip, public_ip, port, name = generate_random_node_data()
 
     # Register new node and check that it successfully created on contracts
     with mock.patch('core.node.update_filebeat_service'):
-        res = node.register(
+        res = unregistered_node.register(
             ip,
             public_ip,
             port,
@@ -58,14 +108,14 @@ def test_register_info(node):
     assert res['status'] == 'ok'
     res_data = res.get('data')
 
-    skalepy_data = node.skale.nodes.get_by_name(name)
+    skalepy_data = unregistered_node.skale.nodes.get_by_name(name)
     assert skalepy_data['name'] == name
-    assert node.skale.nodes.get(res_data['node_id'])
-    assert res_data['node_id'] == node.config.id
+    assert unregistered_node.skale.nodes.get(res_data['node_id'])
+    assert res_data['node_id'] == unregistered_node.config.id
 
     # Register the same node again
-    old_config_id = node.config.id
-    res = node.register(
+    old_config_id = unregistered_node.config.id
+    res = unregistered_node.register(
         ip,
         public_ip,
         port,
@@ -73,50 +123,21 @@ def test_register_info(node):
         domain_name=DEFAULT_DOMAIN_NAME
     )
     assert res['status'] == 'error'
-    assert node.config.id == old_config_id
+    assert unregistered_node.config.id == old_config_id
 
     # Test info
-    info = node.info
+    info = unregistered_node.info
     assert info['name'] == name
     assert info['ip'] == ip
     assert info['publicIP'] == public_ip
     assert info['port'] == int(port)
-    assert info['id'] == node.config.id
-    assert info['publicKey'] == node.skale.wallet.public_key
+    assert info['id'] == unregistered_node.config.id
+    assert info['publicKey'] == unregistered_node.skale.wallet.public_key
     assert info['status'] == 0
 
 
 @pytest.fixture
-def active_node(skale):
-    ip, public_ip, port, name = generate_random_node_data()
-    skale.manager.create_node(
-        ip=ip,
-        port=port,
-        name=name,
-        public_ip=public_ip,
-        domain_name=DEFAULT_DOMAIN_NAME,
-        wait_for=True
-    )
-    config = NodeConfig()
-    config.name = name
-    node_id = skale.nodes.node_name_to_index(name)
-    config.id = node_id
-    config.ip = ip
-    config.name = name
-    try:
-        yield Node(skale, config)
-    finally:
-        status = skale.nodes.get_node_status(node_id)
-        if status not in (NodeStatus.FROZEN.value, NodeStatus.LEFT.value):
-            if status == NodeStatus.IN_MAINTENANCE.value:
-                skale.nodes.remove_node_from_in_maintenance(node_id)
-            skale.nodes.init_exit(node_id)
-            skale.manager.node_exit(node_id)
-
-
-@pytest.fixture
-def maintenance_node(skale, active_node):
-    node = active_node
+def maintenance_node(skale, node):
     try:
         skale.nodes.set_node_in_maintenance(node.config.id)
         yield node
@@ -124,22 +145,23 @@ def maintenance_node(skale, active_node):
         skale.nodes.remove_node_from_in_maintenance(node.config.id)
 
 
-def test_get_node_id_active_node(active_node):
-    node_id = active_node.get_node_id_from_contracts(
-        active_node.config.name, active_node.config.ip
+def test_get_node_id_node(node):
+    node_id = node.get_node_id_from_contracts(
+        node.config.name,
+        node.config.ip
     )
-    assert node_id == active_node.config.id
+    assert node_id == node.config.id
 
 
 @pytest.fixture
-def no_address_node(active_node):
-    acc = generate_account(active_node.skale.web3)
-    address = active_node.skale.wallet.address
-    active_node.skale.wallet._address = acc['address']
+def no_address_node(node):
+    acc = generate_account(node.skale.web3)
+    address = node.skale.wallet.address
+    node.skale.wallet._address = acc['address']
     try:
-        yield active_node
+        yield node
     finally:
-        active_node.skale.wallet._address = address
+        node.skale.wallet._address = address
 
 
 def test_get_node_id_ignores_not_matched_address(no_address_node):
@@ -150,17 +172,17 @@ def test_get_node_id_ignores_not_matched_address(no_address_node):
 
 
 @pytest.fixture
-def no_id_node(active_node):
+def no_id_node(node):
     no_id_config_path = os.path.join(NODE_DATA_PATH, 'no_id_config.json')
     config = NodeConfig(no_id_config_path)
-    config.name = active_node.config.name
-    config.sgx_key_name = active_node.config.sgx_key_name
-    config.ip = active_node.config.ip
-    config, active_node.config = active_node.config, config
+    config.name = node.config.name
+    config.sgx_key_name = node.config.sgx_key_name
+    config.ip = node.config.ip
+    config, node.config = node.config, config
     try:
-        yield active_node
+        yield node
     finally:
-        config, active_node.config = active_node.config, config
+        config, node.config = node.config, config
 
 
 def test_get_node_id_restores_no_id_node(no_id_node):
@@ -170,40 +192,34 @@ def test_get_node_id_restores_no_id_node(no_id_node):
     assert no_id_node.skale.nodes.get(nid)['name'] == no_id_node.config.name
 
 
-@pytest.fixture
-def not_registered_node(skale):
-    config = NodeConfig()
-    return Node(skale, config)
-
-
-def test_get_node_id_node_not_registered(not_registered_node):
-    nid = not_registered_node.get_node_id_from_contracts(
+def test_get_node_id_node_not_registered(unregistered_node):
+    nid = unregistered_node.get_node_id_from_contracts(
         'undefined_name', '0.0.0.0')
     assert nid == -1
 
 
-def test_start_exit(skale, active_node):
-    skale.nodes.init_exit(active_node.config.id)
-    active_node.exit({})
+def test_start_exit(skale, node):
+    skale.nodes.init_exit(node.config.id)
+    node.exit({})
     status = NodeExitStatus(
-        active_node.skale.nodes.get_node_status(active_node.config.id))
+        node.skale.nodes.get_node_status(node.config.id))
 
     assert status != NodeExitStatus.ACTIVE
 
 
-def test_exit_status_active_forzen(skale, active_node):
-    active_status_data = active_node.get_exit_status()
+def test_exit_status_active_forzen(skale, node):
+    active_status_data = node.get_exit_status()
     assert list(active_status_data.keys()) == ['status', 'data', 'exit_time']
     assert active_status_data['status'] == NodeExitStatus.ACTIVE.name
     assert active_status_data['exit_time'] == 0
 
-    skale.nodes.init_exit(active_node.config.id)
-    active_node.exit({})
-    exit_status_data = active_node.get_exit_status()
+    skale.nodes.init_exit(node.config.id)
+    node.exit({})
+    exit_status_data = node.get_exit_status()
     assert list(exit_status_data.keys()) == ['status', 'data', 'exit_time']
     assert exit_status_data['status'] == NodeExitStatus.WAIT_FOR_ROTATIONS.name
     assert exit_status_data['exit_time'] != 0
-    assert active_node.info['status'] == NodeStatus.FROZEN.value
+    assert node.info['status'] == NodeStatus.FROZEN.value
 
 
 def test_exit_status_maintenance(skale, maintenance_node):
@@ -213,29 +229,29 @@ def test_exit_status_maintenance(skale, maintenance_node):
     assert node_data['exit_time'] == 0
 
 
-def test_node_maintenance(active_node, skale):
-    res = active_node.set_maintenance_on()
+def test_node_maintenance(node, skale):
+    res = node.set_maintenance_on()
     assert res == {'data': None, 'status': 'ok'}
     node_status = NodeStatus(
-        skale.nodes.get_node_status(active_node.config.id))
+        skale.nodes.get_node_status(node.config.id))
     assert node_status == NodeStatus.IN_MAINTENANCE
 
-    res = active_node.set_maintenance_off()
+    res = node.set_maintenance_off()
     assert res == {'data': None, 'status': 'ok'}
     node_status = NodeStatus(
-        skale.nodes.get_node_status(active_node.config.id))
+        skale.nodes.get_node_status(node.config.id))
     assert node_status == NodeStatus.ACTIVE
 
 
-def test_node_maintenance_error(active_node, skale):
-    res = active_node.set_maintenance_off()
+def test_node_maintenance_error(node, skale):
+    res = node.set_maintenance_off()
     assert res == {'status': 'error',
                    'errors': ['Node is not in maintenance mode']}
 
-    res = active_node.set_maintenance_on()
+    res = node.set_maintenance_on()
     assert res == {'data': None, 'status': 'ok'}
     with pytest.raises(RevertError):
-        res = active_node.set_maintenance_on()
+        res = node.set_maintenance_on()
 
 
 @mock.patch('core.node.get_block_device_size', return_value=300)
