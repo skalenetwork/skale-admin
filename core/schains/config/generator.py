62d213e9afa304e5a2db33592d60f7161ddd11ec
badrogger <ivan@skalelabs.com>
2022-11-22 17:36:23 +0000

    Move patches timestamps to sChain section

--- core/schains/config/generator.py
+++ core/schains/config/generator.py
@@ -70,7 +70,6 @@ class SChainConfig:
     unddos: dict
     genesis: dict
     accounts: dict
-    patches: dict
     skale_config: SkaleConfig
 
     def to_dict(self):
@@ -81,7 +80,6 @@ class SChainConfig:
             'unddos': self.unddos,
             'genesis': self.genesis,
             'accounts': self.accounts,
-            'patches': self.patches,
             'skaleConfig': self.skale_config.to_dict(),
         }
 
@@ -183,7 +181,6 @@ def generate_schain_config(
         },
         unddos=base_config.config['unddos'],
         genesis=base_config.config['genesis'],
-        patches=base_config.config['patches'],
         accounts={
             **base_config.config['accounts'],
             **predeployed_accounts,
