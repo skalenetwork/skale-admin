502d4297890789df976fd187e2ea53d849273925
badrogger <ivan@skalelabs.com>
2023-01-06 19:29:55 +0000

    Improve schedule_exit test

--- tests/schains/monitor/containers_test.py
+++ tests/schains/monitor/containers_test.py
@@ -1,5 +1,7 @@
-import mock
+import datetime
 
+import mock
+import freezegun
 from skale.schain_config.generator import get_nodes_for_schain
 
 from core.schains.monitor.containers import monitor_schain_container, schedule_exit
@@ -8,6 +10,9 @@ from web.models.schain import upsert_schain_record
 
 from tests.schains.monitor.main_test import run_exited_schain_container
 from tests.utils import request_mock, response_mock
+
+CURRENT_TIMESTAMP = 1594903080
+CURRENT_DATETIME = datetime.datetime.utcfromtimestamp(CURRENT_TIMESTAMP)
 
 
 def test_monitor_schain_container(
@@ -129,6 +134,7 @@ def test_monitor_schain_container_ec_0(
         assert schain_record.failed_rpc_count == 100
 
 
+@freezegun.freeze_time(CURRENT_DATETIME)
 def test_schedule_exit(
     skale,
     node_config,
@@ -138,9 +144,13 @@ def test_schedule_exit(
     schain_name = schain_on_contracts
     node_id = node_config.id
     schain_nodes = get_nodes_for_schain(skale, schain_name)
-    print(schain_nodes)
 
+    post_mock = request_mock(response_mock(json_data={}))
     with mock.patch('core.schains.monitor.containers.requests.post',
-                    request_mock(response_mock)):
+                    post_mock):
         schedule_exit(schain_name, schain_nodes, node_id)
-        post_mock.assert_called_with([])
+        post_mock.assert_called_with(
+            url='http://127.0.0.1:10003',
+            data='{"id": 0, "jsonrpc": "2.0", "method": "setSchainExitTime", "params": {"finishTime": 1594903500}}',  # noqa
+            headers={'content-type': 'application/json'}
+        )
