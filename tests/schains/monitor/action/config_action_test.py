9a9a9d527e6d2c800c3d8a6c3c73069d6eb6e974
badrogger <ivan@skalelabs.com>
2023-06-10 16:29:26 +0000

    Fix config actions tests

--- tests/schains/monitor/action/config_action_test.py
+++ tests/schains/monitor/action/config_action_test.py
@@ -1,6 +1,9 @@
+import shutil
+
 import pytest
 
 from core.schains.checks import ConfigChecks
+from core.schains.config.directory import schain_config_dir
 from core.schains.monitor.action import ConfigActionManager
 
 from web.models.schain import SChainRecord
@@ -51,12 +54,28 @@ def config_am(
     )
 
 
-def test_config_actions(config_am, config_checks):
+def test_upstream_config_actions(config_am, config_checks):
     config_am.config_dir()
-    assert config_checks.config_dir.status
-    assert not config_checks.config.status
+    assert config_checks.config_dir
+    assert not config_checks.upstream_config
+
+    # Folder created for secret key. Temporary moving
+    schain_folder = schain_config_dir(config_am.name)
+    tmp_schain_folder = '.' + schain_folder
+    try:
+        shutil.move(schain_folder, tmp_schain_folder)
+        assert not config_checks.config_dir
+        assert not config_checks.upstream_config
+    finally:
+        shutil.move(tmp_schain_folder, schain_folder)
 
     # DKG action is tested separetely in dkg_test module
 
-    config_am.config()
-    assert config_checks.config.status
+    config_am.config_dir()
+    config_am.upstream_config()
+    assert config_checks.config_dir
+    assert config_checks.upstream_config
+
+    # Try to recreate config with no changes
+    config_am.upstream_config()
+    assert config_checks.upstream_config
