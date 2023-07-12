293d2f0d821a86fdf138a1d0a60bc63506d81d51
badrogger <ivan@skalelabs.com>
2023-07-12 14:54:45 +0000

    Fix ima action tests

--- tests/schains/config/config_test.py
+++ tests/schains/config/config_test.py
@@ -11,7 +11,7 @@ from core.schains.config.helper import (
     get_schain_env
 )
 from core.schains.config.directory import get_upstream_config_filepath, schain_config_dir
-from core.schains.config.main import get_finish_ts, get_rotation_ids_from_config
+from core.schains.config.main import get_latest_finish_ts, get_rotation_ids_from_config
 from core.schains.volume import get_schain_volume_config
 from tools.configs.containers import SHARED_SPACE_CONTAINER_PATH, SHARED_SPACE_VOLUME_NAME
 
@@ -80,12 +80,18 @@ def test_get_schain_upstream_config(schain_db, upstreams):
     assert upstream_config is None
 
 
-def test_get_finish_ts(schain_config):
-    finish_ts = get_finish_ts(schain_config)
-    assert finish_ts == 1687180291
+def test_get_latest_finish_ts(schain_config):
+    schain_config['skaleConfig']['sChain']['nodeGroups'].update(
+        {
+            '2': {'finish_ts': None},
+            '3': {'finish_ts': None}
+        }
+    )
 
+    finish_ts = get_latest_finish_ts(schain_config)
+    assert finish_ts == 1687180291
     schain_config['skaleConfig']['sChain']['nodeGroups'].pop('0')
-    finish_ts = get_finish_ts(schain_config)
+    finish_ts = get_latest_finish_ts(schain_config)
     assert finish_ts is None
 
 
