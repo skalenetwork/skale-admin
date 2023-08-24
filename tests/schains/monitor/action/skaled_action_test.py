b313eeab581d93441b0b58741d4774b45d3190eb
badrogger <ivan@skalelabs.com>
2023-08-24 16:32:54 +0000

    Fix skaled_container action test

--- tests/schains/monitor/action/skaled_action_test.py
+++ tests/schains/monitor/action/skaled_action_test.py
@@ -39,6 +39,7 @@ def monitor_schain_container_mock(
     skaled_status,
     download_snapshot=False,
     start_ts=None,
+    restart_on_exit=True,
     dutils=None
 ):
     image_name, container_name, _, _ = get_container_info(
@@ -132,6 +133,7 @@ def test_skaled_container_with_snapshot_action(skaled_am):
             skaled_status=skaled_am.skaled_status,
             download_snapshot=True,
             start_ts=None,
+            restart_on_exit=True,
             dutils=skaled_am.dutils
         )
         assert monitor_schain_mock.call_count == 1
@@ -155,6 +157,7 @@ def test_skaled_container_snapshot_delay_start_action(skaled_am):
             skaled_status=skaled_am.skaled_status,
             download_snapshot=True,
             start_ts=ts,
+            restart_on_exit=True,
             dutils=skaled_am.dutils
         )
         assert monitor_schain_mock.call_count == 1
