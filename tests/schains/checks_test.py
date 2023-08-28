e85c93281ff7622f82afb643743bc7e45428c1da
badrogger <ivan@skalelabs.com>
2023-08-28 10:12:57 +0000

    Run tests only on push

--- tests/schains/checks_test.py
+++ tests/schains/checks_test.py
@@ -186,9 +186,7 @@ def test_ima_container_check(schain_checks, cleanup_ima_containers, dutils):
     schain = get_schain_contracts_data(name)
     image = get_image_name(type=IMA_CONTAINER)
     new_image = get_image_name(type=IMA_CONTAINER, new=True)
-    print('HERE DEBUG', new_image)
 
-    dutils.pull(new_image)
     if dutils.pulled(new_image):
         dutils.rmi(new_image)
 
