20e78146da54269ff5e4c1854599273b873be0c6
badrogger <ivan@skalelabs.com>
2023-10-06 12:49:31 +0000

    Run all check_tests

--- scripts/run_core_tests.sh
+++ scripts/run_core_tests.sh
@@ -13,7 +13,7 @@ export_test_env
 run_sgx_simulator $SGX_WALLET_TAG
 bash scripts/run_redis.sh
 
-py.test --cov-config=.coveragerc --cov=. tests/ --ignore=tests/firewall -k test_ima_container_check $@
+py.test --cov-config=.coveragerc --cov=. tests/checks_test.py --ignore=tests/firewall $@
 tests_cleanup
 # scripts/run_firewall_test.sh
 # tests_cleanup
