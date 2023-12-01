2503849faadfeb1573b31518f405203cfcd1b488
badrogger <ivan@skalelabs.com>
2023-12-01 14:00:50 +0000

    Remove unsued get_schain_public_key function

--- core/schains/exit_scheduler.py
+++ core/schains/exit_scheduler.py
@@ -50,13 +50,3 @@ class ExitScheduleFileManager:
         with open(tmp_path, 'w') as filepath:
             json.dump({'timestamp': ts}, filepath)
         shutil.move(tmp_path, self.path)
-
-
-def get_schain_public_key(skale, schain_name):
-    group_idx = skale.schains.name_to_id(schain_name)
-    raw_public_key = skale.key_storage.get_previous_public_key(group_idx)
-    public_key_array = [*raw_public_key[0], *raw_public_key[1]]
-    if public_key_array == ['0', '0', '1', '0']:  # zero public key
-        raw_public_key = skale.key_storage.get_common_public_key(group_idx)
-        public_key_array = [*raw_public_key[0], *raw_public_key[1]]
-    return ':'.join(map(str, public_key_array))
