e50473556bbc9972103d08cbaba1b58dcb873721
badrogger <ivan@skalelabs.com>
2024-05-28 15:26:43 +0000

    Fix static_groups test

--- tests/schains/config/static_accounts_test.py
+++ tests/schains/config/static_accounts_test.py
@@ -1,6 +1,73 @@
+import os
+import shutil
+
+import pytest
+
 from core.schains.config.legacy_data import is_static_accounts, static_accounts, static_groups
+from tools.configs import ENV_TYPE, STATIC_GROUPS_FOLDER
+from tools.helper import write_json
+
 
 SCHAIN_NAME = 'test'
+
+STATIC_NODE_GROUPS = {
+    '1': {
+        "rotation": {
+            "leaving_node_id": 3,
+            "new_node_id": 4,
+        },
+        "nodes": {
+            "0": [
+                0,
+                159,
+                "0xgd"
+            ],
+            "4": [
+                4,
+                31,
+                "0x5d"
+            ],
+        },
+        "finish_ts": None,
+        "bls_public_key": None
+    },
+    '0': {
+        "rotation": {
+            "leaving_node_id": 2,
+            "new_node_id": 3,
+        },
+        "nodes": {
+            "0": [
+                0,
+                159,
+                "0xgd"
+            ],
+            "3": [
+                7,
+                61,
+                "0xbh"
+            ],
+        },
+        "finish_ts": 1681390775,
+        "bls_public_key": {
+            "blsPublicKey0": "3",
+            "blsPublicKey1": "4",
+            "blsPublicKey2": "7",
+            "blsPublicKey3": "9"
+        }
+    }
+}
+
+
+@pytest.fixture
+def groups_for_chain():
+    parent_folder = os.path.join(STATIC_GROUPS_FOLDER, ENV_TYPE)
+    os.makedirs(parent_folder)
+    static_groups_env_path = os.path.join(parent_folder, os.path.join(f'schain-{SCHAIN_NAME}.json'))
+    try:
+        yield write_json(static_groups_env_path, STATIC_NODE_GROUPS)
+    finally:
+        shutil.rmtree(STATIC_GROUPS_FOLDER, ignore_errors=True)
 
 
 def test_is_static_accounts():
@@ -14,6 +81,6 @@ def test_static_accounts():
     assert accounts.get('accounts', None)
 
 
-def test_static_groups():
-    assert static_groups(SCHAIN_NAME)
+def test_static_groups(groups_for_chain):
+    assert static_groups(SCHAIN_NAME) == STATIC_NODE_GROUPS
     assert static_groups('not-exists') == {}
