b89c4ee03b330fe2725cf99a3ccaa16ba9e79efc
badrogger <ivan@skalelabs.com>
2024-07-11 11:17:44 +0000

    Add tests for no_hyphens

--- tests/helper_test.py
+++ tests/helper_test.py
@@ -1,4 +1,4 @@
-from tools.helper import is_address_contract
+from tools.helper import is_address_contract, no_hyphens
 from tools.configs.web3 import ZERO_ADDRESS
 
 
@@ -6,3 +6,10 @@ def test_is_address_contract(skale):
     assert not is_address_contract(skale.web3, ZERO_ADDRESS)
     assert is_address_contract(skale.web3, skale.manager.address)
     assert is_address_contract(skale.web3, skale.nodes.address)
+
+
+def test_no_hyphen():
+    no_hyphens('too') == 'too'
+    no_hyphens('too-boo') == 'too_boo'
+    no_hyphens('too-boo_goo') == 'too_boo_goo'
+    no_hyphens('too_goo') == 'too_goo'
