6998fbc801b2103111bff2cbb834981e0f54fced
badrogger <ivan@skalelabs.com>
2024-08-08 15:06:33 +0000

    Add retry to image pulling

--- scripts/run_core_tests.sh
+++ scripts/run_core_tests.sh
@@ -13,5 +13,5 @@ export_test_env
 run_sgx_simulator $SGX_WALLET_TAG
 bash scripts/run_redis.sh
 
-py.test --cov-config=.coveragerc --cov=. tests/schains/monitor/action/skaled_action_test.py --ignore=tests/firewall $@
+py.test --cov-config=.coveragerc --cov=. tests/ --ignore=tests/firewall $@
 tests_cleanup
