b765396883dbe52f310209e21053fa648eb1d7c8
badrogger <ivan@skalelabs.com>
2024-08-08 14:17:15 +0000

    Run only action tests

--- scripts/run_core_tests.sh
+++ scripts/run_core_tests.sh
@@ -11,7 +11,7 @@ tests_cleanup
 export_test_env
 
 run_sgx_simulator $SGX_WALLET_TAG
-# bash scripts/run_redis.sh
+bash scripts/run_redis.sh
 
-py.test --cov-config=.coveragerc --cov=. tests/ --ignore=tests/firewall $@
+py.test --cov-config=.coveragerc --cov=. tests/schains/monitor/action/skaled_action_test.py --ignore=tests/firewall $@
 tests_cleanup
