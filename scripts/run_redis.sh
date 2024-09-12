0a4e8600ac4db594543ad3040ed539c2984f4bf9
badrogger <ivan@skalelabs.com>
2024-09-12 11:50:13 +0000

    Fix tests

--- scripts/run_redis.sh
+++ scripts/run_redis.sh
@@ -2,8 +2,4 @@ set -e
 
 docker rm -f redis || true
 export DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-<<<<<<< HEAD
-docker run -v $DIR/../tests/redis-conf:/config:Z --network=host --name=redis -d redis:6.0-alpine
-=======
-docker run -v $DIR/../tests/redis-conf:/config -p 6381:6381 --name=redis -d redis:6.0-alpine
->>>>>>> develop
+docker run -v $DIR/../tests/redis-conf:/config --network=host --name=redis -d redis:6.0-alpine
--- tests/monitoring_test.py
+++ tests/monitoring_test.py
@@ -1,12 +1,11 @@
+import json
 import os
 
 import pytest
 
 from core.monitoring import TelegrafNotConfiguredError, update_telegraf_service
-from tools.configs.monitoring import (
-    TELEGRAF_TEMPLATE_PATH,
-    TELEGRAF_CONFIG_PATH
-)
+from tools.configs import DOCKER_NODE_CONFIG_FILEPATH
+from tools.configs.monitoring import TELEGRAF_TEMPLATE_PATH, TELEGRAF_CONFIG_PATH
 
 
 CONFIG_TEMPLATE = """
@@ -23,6 +22,8 @@ CONFIG_TEMPLATE = """
   urls = ["{{ url }}"]
 
 """
+
+DOCKER_GROUP_ID = 1023
 
 
 @pytest.fixture
@@ -44,26 +45,29 @@ def telegraf_template():
         os.remove(TELEGRAF_CONFIG_PATH)
 
 
-def test_update_telegraf_service(telegraf_template, cleanup_container, dutils):
+@pytest.fixture
+def docker_node_config():
+    try:
+        with open(DOCKER_NODE_CONFIG_FILEPATH, 'w') as docker_config:
+            json.dump({'docker_group_id': DOCKER_GROUP_ID}, docker_config)
+        yield DOCKER_NODE_CONFIG_FILEPATH
+    finally:
+        os.remove(DOCKER_NODE_CONFIG_FILEPATH)
+
+
+def test_update_telegraf_service(docker_node_config, telegraf_template, cleanup_container, dutils):
     node_id = 1
     node_ip = '1.1.1.1'
     with pytest.raises(TelegrafNotConfiguredError):
         update_telegraf_service(
-            node_id=node_id,
-            node_ip='',
-            url='http://127.0.0.1:1231',
-            dutils=dutils
+            node_id=node_id, node_ip='', url='http://127.0.0.1:1231', dutils=dutils
         )
 
-    update_telegraf_service(
-        node_ip,
-        node_id,
-        url='http://127.0.0.1:1231',
-        dutils=dutils
-    )
+    update_telegraf_service(node_ip, node_id, url='http://127.0.0.1:1231', dutils=dutils)
     with open(TELEGRAF_CONFIG_PATH) as config:
         config = config.read()
-        assert config == '\n[agent]\n  interval = "60s"\n  hostname = "1.1.1.1"\n  omit_hostname = false\n\n[global_tags]\n  node_id = "1"\n\n[[outputs.db]]\n  alias = "db"\n  urls = ["http://127.0.0.1:1231"]\n'  # noqa
+        assert (
+            config == '\n[agent]\n  interval = "60s"\n  hostname = "1.1.1.1"\n  omit_hostname = false\n\n[global_tags]\n  node_id = "1"\n\n[[outputs.db]]\n  alias = "db"\n  urls = ["http://127.0.0.1:1231"]\n') # noqa
     assert dutils.is_container_running('skale_telegraf')
-    print(dutils.get_info('skale_telegraf')['stats'])
-    assert dutils.get_info('skale_telegraf')['stats']['Config']['User'] == 'telegraf:998'
+    user_info = dutils.get_info('skale_telegraf')['stats']['Config']['User']
+    assert user_info == f'telegraf:{DOCKER_GROUP_ID}'
