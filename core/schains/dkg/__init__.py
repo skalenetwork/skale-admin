20fd133e36f8b1d8b4ec4795b1dd39214a3ae1a5
André Gonçalves <andreafonsogoncalves2001@gmail.com>
2025-03-10 11:13:57 +0000

    Add ruff to test pipeline for linting and formatting checks. Fixed some linting issues on dkg init.py.

--- core/schains/dkg/__init__.py
+++ core/schains/dkg/__init__.py
@@ -19,6 +19,6 @@
 
 # flake8: noqa: E402
 
-from core.schains.dkg.main import get_dkg_client, run_dkg, save_dkg_results
-from core.schains.dkg.structures import DKGStatus, DKGStep
-from core.schains.dkg.utils import DkgError, get_secret_key_share_filepath
+from core.schains.dkg.main import get_dkg_client, run_dkg, save_dkg_results  # noqa
+from core.schains.dkg.structures import DKGStatus, DKGStep  # noqa
+from core.schains.dkg.utils import DkgError, get_secret_key_share_filepath  # noqa
