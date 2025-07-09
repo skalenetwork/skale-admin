820dda681cff64e48753e4d4424e982e95f606d2
Oleh <oleg@skalelabs.com>
2025-07-09 19:09:34 +0100

    IS 1182 fix import

--- core/dkg/mirage/__init__.py
+++ core/dkg/mirage/__init__.py
@@ -20,5 +20,5 @@
 # ruff: noqa: F401
 
 from core.dkg.mirage.main import get_dkg_client, run_dkg, save_dkg_results
-from core.dkg.mirage.utils import DkgError, get_secret_key_share_filepath
 from core.dkg.structures import DKGStatus, DKGStep
+from core.dkg.utils import DkgError, get_secret_key_share_filepath
--- core/dkg/schain/__init__.py
+++ core/dkg/schain/__init__.py
@@ -20,5 +20,5 @@
 # ruff: noqa: F401
 
 from core.dkg.schain.main import get_dkg_client, run_dkg, save_dkg_results
-from core.dkg.schain.structures import DKGStatus, DKGStep
-from core.dkg.schain.utils import DkgError, get_secret_key_share_filepath
+from core.dkg.structures import DKGStatus, DKGStep
+from core.dkg.utils import DkgError, get_secret_key_share_filepath
