cf62d9db12a342b533dbdf42bb5d2e295223f6e4
badrogger <ivan@skalelabs.com>
2025-07-09 21:13:26 +0100

    Extract skaled tag

--- scripts/run_skaled.sh
+++ scripts/run_skaled.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 set -ea
+
+: "${SKALED_TAG?Need to set SKALED_TAG}"
+
 export DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 if [ ! -d "$DIR/../tests/schain-data/.artifacts" ]; then
   mkdir -p "$DIR/../tests/schain-data/.artifacts"
@@ -13,7 +16,7 @@ docker run -d \
   -v "$DIR/../tests/schain-data/.artifacts/skaled-datadir":"/skaled/datadir" \
   -v "$DIR/../tests/schain-data/mirage-conf":"/skaled/config" \
   --ulimit nofile=65535:65535 \
-  skalenetwork/schain:4.1.0-develop.24-mirage \
+  skalenetwork/schain:${SKALED_TAG} \
   --config config/skaled-config.json \
   --http-port 1234 \
   --ws-port 1233 \
