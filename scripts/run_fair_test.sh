e466a9f511fdcf1cb656cfae463adf5fe59c32fd
badrogger <ivan@skalelabs.com>
2025-08-11 17:04:04 +0100

    Do not cleanup sgx before tests

--- scripts/run_fair_test.sh
+++ scripts/run_fair_test.sh
@@ -8,7 +8,7 @@ source $DIR/../helper-scripts/helper.sh
 : "${SGX_WALLET_TAG?Need to set SGX_WALLET_TAG}"
 
 tests_cleanup
-sgx_cleanup
+# sgx_cleanup
 export_test_env
 
 # run_sgx_simulator $SGX_WALLET_TAG
@@ -16,4 +16,3 @@ bash scripts/run_redis.sh
 
 python -m py.test tests/fair/dkg_test.py $@
 sgx_cleanup
-
