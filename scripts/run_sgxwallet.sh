a4fe3c4f74e28ca28839a1c97f43e8e380bd7e3e
Dmytro <dmitry.tkk@gmail.com>
2025-12-29 17:38:58 +0000

    Update sgx wallet tag

--- scripts/run_sgxwallet.sh
+++ scripts/run_sgxwallet.sh
@@ -8,5 +8,6 @@ source $DIR/../helper-scripts/helper.sh
 : "${SGX_WALLET_TAG?Need to set SGX_WALLET_TAG}"
 
 sgx_cleanup
-run_sgx_simulator $SGX_WALLET_TAG
+#run_sgx_simulator $SGX_WALLET_TAG
+docker run -d -p 1026-1031:1026-1031 --name $SGX_WALLET_CONTAINER_NAME --entrypoint /bin/bash $SGX_WALLET_IMAGE_NAME -c "source /opt/intel/sgxsdk/environment && cd /usr/src/sdk && ./sgxwallet -y -n"
 bash scripts/run_redis.sh
