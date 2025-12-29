a3bf387f47bae8e6b7b3b887f8edcd585f2da5e8
Dmytro <dmitry.tkk@gmail.com>
2025-12-29 18:03:49 +0000

    Fix run sgx sim script

--- scripts/run_sgxwallet.sh
+++ scripts/run_sgxwallet.sh
@@ -9,5 +9,6 @@ source $DIR/../helper-scripts/helper.sh
 
 sgx_cleanup
 #run_sgx_simulator $SGX_WALLET_TAG
-docker run -d -p 1026-1031:1026-1031 --name sgx-simulator --entrypoint /bin/bash $SGX_WALLET_TAG -c "source /opt/intel/sgxsdk/environment && cd /usr/src/sdk && ./sgxwallet -y -n"
+SGX_WALLET_IMAGE_NAME=skalenetwork/sgxwallet_sim:$SGX_WALLET_TAG
+docker run -d -p 1026-1031:1026-1031 --name sgx-simulator --entrypoint /bin/bash $SGX_WALLET_IMAGE_NAME -c "source /opt/intel/sgxsdk/environment && cd /usr/src/sdk && ./sgxwallet -y -n"
 bash scripts/run_redis.sh
