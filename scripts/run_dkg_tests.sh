2c1fe311a669c8a0630ff7770631043a9a59d26c
Dmytro <dmitry.tkk@gmail.com>
2026-01-13 19:47:18 +0000

    Add dkg tests pipeline

---
+++ scripts/run_dkg_tests.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+set -ea
+
+export DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+source $DIR/helper.sh
+source $DIR/../helper-scripts/helper.sh
+
+: "${SGX_WALLET_TAG?Need to set SGX_WALLET_TAG}"
+
+tests_cleanup
+
+export_test_env
+
+bash scripts/run_redis.sh
+
+uv run pytest --cov-config=.coveragerc --cov=. tests/dkg_test $@
+tests_cleanup
+sgx_cleanup
