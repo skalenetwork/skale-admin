7c85b3ed052c8026c53bac09020f20b6e0f101a7
Dmytro <dmitry.tkk@gmail.com>
2026-01-14 13:17:34 +0000

    Use sgx sim image from ghcr in test pipeline

--- scripts/run_sgxwallet.sh
+++ scripts/run_sgxwallet.sh
@@ -9,6 +9,6 @@ source $DIR/../helper-scripts/helper.sh
 
 sgx_cleanup
 #run_sgx_simulator $SGX_WALLET_TAG
-SGX_WALLET_IMAGE_NAME=skalenetwork/sgxwallet_sim:$SGX_WALLET_TAG
+SGX_WALLET_IMAGE_NAME=ghcr.io/skalenetwork/sgxwallet_sim:$SGX_WALLET_TAG
 docker run -d -p 1026-1031:1026-1031 --name sgx-simulator --entrypoint /bin/bash $SGX_WALLET_IMAGE_NAME -c "source /opt/intel/sgxsdk/environment && cd /usr/src/sdk && ./sgxwallet -s -y"
 bash scripts/run_redis.sh
